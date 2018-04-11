c91d5232843e859c31c1bc9a2ea07a1627395a46
sylvie chassery <sylviechassery@gmail.com>
2018-04-11 15:47:54 +0200

    Ajout Photos Voyage

---
+++ Projet_Final/Insert_Continents.sql
@@ -0,0 +1,9 @@
+USE [FinalDB]
+GO
+INSERT INTO [dbo].Continent (Nom) 
+VALUES ('Afrique'),
+	   ('Amérique'),
+	   ('Asie'),
+	   ('Europe'),
+	   ('Océanie')
+GO
---
+++ Projet_Final/Insert_Pays.sql
@@ -0,0 +1,19 @@
+USE [FinalDB]
+GO
+INSERT INTO [dbo].Pays (Nom,IdContinent) 
+VALUES ('Maroc','1'),
+	   ('Sénégal','1'),
+	   ('Tunisie','1'),
+	   ('US','2'),
+	   ('Canada','2'),
+	   ('Mexique','2'),
+	   ('Thailande','3'),
+	   ('Japon','3'),
+	   ('Inde','3'),
+	   ('France','4'),
+	   ('Italie','4'),
+	   ('Espagne','4'),
+	   ('Australie','5'),
+	   ('Fidji','5'),
+	   ('Nauru','5')
+GO
